86a92e95e4edb751f5d13da5fc8f2ef0ac3fa869
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-08-21 20:34:57 +0200

    adding some view Club and Race

---
+++ GroopWebApp/Controllers/ClubController.cs
@@ -0,0 +1,19 @@
+﻿using GroopWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroopWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+    }
+}
---
+++ GroopWebApp/Controllers/RaceController.cs
@@ -0,0 +1,20 @@
+﻿using GroopWebApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroopWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var races = _context.Races.ToList();
+            return View(races);
+        }
+    }
+}
