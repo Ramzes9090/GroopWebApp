15d8cb53df6aaa711b9326aba30dd7060e576a86
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-08-23 18:06:43 +0200

    added edit operation to club and race

---
+++ GroopWebApp/ViewModels/EditClubViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GroopWebApp.Data.Enum;
+using GroopWebApp.Models;
+
+namespace GroopWebApp.ViewModels
+{
+    public class EditClubViewModel
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string? URL { get; set; }
+        public IFormFile Image { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        public ClubCategory ClubCategory { get; set; }
+    }
+}
