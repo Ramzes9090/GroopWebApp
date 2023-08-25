a1e99e230bc5c35f996364c721e213024ebad74d
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-08-25 22:44:50 +0200

    Adding edit user profile

--- GroopWebApp/Controllers/DashboardController.cs
+++ GroopWebApp/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
-﻿using GroopWebApp.Data;
+﻿using CloudinaryDotNet.Actions;
+using GroopWebApp.Data;
 using GroopWebApp.Interfaces;
+using GroopWebApp.Models;
 using GroopWebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +10,23 @@ namespace GroopWebApp.Controllers
     public class DashboardController : Controller
     {
         private readonly IDashboardRepository _dashboardRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IPhotoService _photoService;
 
-        public DashboardController(IDashboardRepository dashboardRepository)
+        public DashboardController(IDashboardRepository dashboardRepository, IHttpContextAccessor httpContextAccessor, IPhotoService photoService)
         {
             _dashboardRepository = dashboardRepository;
+            _httpContextAccessor = httpContextAccessor;
+            _photoService = photoService;
+        }
+        private void MapUserEdit(AppUser user, EditUserDashboardViewModel editVM, ImageUploadResult photoResult)
+        {
+            user.Id = editVM.Id;
+            user.Pace = editVM.Pace;
+            user.Mileage = editVM.Mileage;
+            user.City = editVM.City;
+            user.ProfileImageUrl = photoResult.Url.ToString();
+            user.State = editVM.State;
         }
         public async Task<IActionResult> Index()
         {
@@ -25,6 +40,61 @@ namespace GroopWebApp.Controllers
             };
 
             return View(dashboardViewModel);
+        }
+
+        public async Task<IActionResult> EditUserProfile()
+        {
+            var curUserId = _httpContextAccessor.HttpContext.User.GetUserId();
+            var user = await _dashboardRepository.GetUserById(curUserId);
+            if (user == null)
+                return View("Error");
+            var editUserViewModel = new EditUserDashboardViewModel()
+            {
+                Id = curUserId,
+                Pace = user.Pace,
+                Mileage = user.Mileage,
+                ProfileImageUrl = user.ProfileImageUrl,
+                City = user.City,
+                State = user.State
+            };
+            return View(editUserViewModel);
+
+            
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditUserProfile(EditUserDashboardViewModel editVM)
+        {
+            if(!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit profile");
+                return View("EditUserProfile", editVM);
+            }
+            var user = await _dashboardRepository.GetByIdNoTracking(editVM.Id);
+
+            if(user.ProfileImageUrl == "" || user.ProfileImageUrl == null)
+            {
+                var photoResult = await _photoService.AddPhotoAsync(editVM.Image);
+
+                MapUserEdit(user,editVM, photoResult);
+                _dashboardRepository.Update(user);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                try
+                {
+                    await _photoService.DeletePhotoAsync(user.ProfileImageUrl);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Could not delete photo");
+                    return View(editVM);
+                }
+                var photoResult = await _photoService.AddPhotoAsync(editVM.Image);
+                MapUserEdit(user, editVM, photoResult);
+                _dashboardRepository.Update(user);
+                return RedirectToAction("Index");
+            }
         }
     }
 }
--- GroopWebApp/Repository/DashboardRepository.cs
+++ GroopWebApp/Repository/DashboardRepository.cs
@@ -1,6 +1,7 @@
 ﻿using GroopWebApp.Data;
 using GroopWebApp.Interfaces;
 using GroopWebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GroopWebApp.Repository
 {
@@ -26,6 +27,25 @@ namespace GroopWebApp.Repository
             var curUser = _httpContextAccessor.HttpContext?.User.GetUserId();
             var userRaces = _context.Races.Where(r => r.AppUser.Id == curUser);
             return userRaces.ToList();
+        }
+
+        public async Task<AppUser> GetUserById(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+        public async Task<AppUser> GetByIdNoTracking(string id)
+        {
+            return await _context.Users.Where(u=>u.Id == id).AsNoTracking().FirstOrDefaultAsync();
+        }
+        public bool Update(AppUser user)
+        {
+            _context.Users.Update(user);
+            return Save();
+        }
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
         }
     }
 }
